8cdaadf6f70f1c54f7c9b47afab15d5d5e779186
jonwhitmer <jonwhitmer23@gmail.com>
2025-05-14 00:27:11 -0400

    Spring Boot Structure #1

---
+++ src/main/java/com/example/backend/FantasyFootballApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class FantasyFootballApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FantasyFootballApplication.class, args);
+    }
+}
